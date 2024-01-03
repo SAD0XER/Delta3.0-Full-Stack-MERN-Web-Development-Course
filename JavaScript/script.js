f104320800fca3305b11e9729a9b1aab4e2bf9eb
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-01-03 16:37:48 +0530

    Solved JS Assignment Question Set (Part 3)

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,68 +1,41 @@
-// JS Assignment Question Set (Direct Part 2)
+// JS Assignment Question Set (Direct Part 3)
 
 //Question 1
-let num = 234;
-
-if (num % 10 === 0) {
-    console.log("Good");
-} else {
-    console.log("Bad");
-}
+let array = [7, 9, 0, -2], n = 3;
+console.log(array.slice(0, n));
 
 //Question 2
-let name = prompt("Enter Your Name:");
-let age = prompt("Enter Your Age:");
-alert(`${name} is ${age} year old.`);
+console.log(array.slice(-n));
 
 //Question 3
-let quarter = 1;
+let arr = [1];
 
-switch (quarter) {
-    case 1:
-        console.log("January, February, March");
-        break;
-    case 2:
-        console.log("April, May, June");
-        break;
-    case 3:
-        console.log("July, August, September");
-        break;
-    case 4:
-        console.log("October, November, December");
-        break;
-    default:
-        console.log("There are only 4 Quarters in 1 year!");
+if (arr.length == 0) {
+    console.log("String is Empty.");
+} else {
+    console.log("String is NOT Empty.");
 }
 
 //Question 4
-let str = "Apna College";
+let charArr = ['a', 'B', 'C'];
 
-if ((str[0] === 'A' || 'a') && (str.length > 5)) {
-    console.log(`"${str}" is a Golden String!`);
+if (charArr[0] == charArr[0].toLocaleLowerCase()) {
+    console.log("Character of the string is Lowercase.");
 } else {
-    console.log(`"${str}" is NOT a Golden String!`);
+    console.log("Character of the string is NOT Lowercase.");
 }
 
 //Question 5
-let num1 = 1;
-let num2 = 12;
-let num3 = 123;
-
-if (num1 > num2) {
-    if (num1 > num3) {
-        console.log(num1 + " is greater than both of the other numbers.");
-    }
-} else if (num2 > num3) {
-    console.log(num2 + " is greater than both of the other numbers.");
-} else {
-    console.log(num3 + " is greater than both of the other numbers.");
-}
+let str = "    as; ldjfd asdf    ";
+console.log(str);
+console.log(str.trim());
 
 //Question 6
-let number1 = 1, number2 = 12;
+console.log(charArr.includes("A"));
+console.log(charArr.includes("B"));
 
-if ((num1 % 10) == (num2 % 10)) {
-    console.log("Both numbers have the same last digit which is", num1 % 10);
+if (charArr.indexOf('C') != -1) {
+    console.log("Element Exist!");
 } else {
-    console.log("Both numbers don't have the same last digit.");
+    console.log("Element Doesn't Exist!");
 }
