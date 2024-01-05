58ed1490b45cd54f96a95b27ec88ac6fd0a9bd6e
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-01-05 16:32:16 +0530

    Solved JS Assignment Question Set (Part 4)

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,33 +1,63 @@
-let todo = [];
+// JS Assignment Question Set (Part 4)
 
-let req = prompt("Please Enter Your Request");
+//Question 1
+let array = [1, 2, 3, 4, 5, 6, 2, 3], number = 2;
 
-while (true) {
-    if (req == "quit") {
-        console.log("Quitting To Do App...");
-        break;
-    } else if (req == 'list') {
-        console.log("----------All Tasks----------");
-        console.log("----------[start]----------");
-        for (let i = 0; i < todo.length; i++) {
-            console.log(i + 1, todo[i]);
-        }
-        console.log("----------[end]----------");
-    } else if (req == 'add') {
-        let task = prompt("Enter Your Task...");
-        todo.push(task);
-        console.log("Task Added!");
-    } else if (req == "delete") {
-        let taskListString = "";
-        for (let i = 0; i < todo.length; i++) {
-            taskListString += `${i + 1}: ${todo[i]}\n`; // Constructing the string
-        }
-        let taskToDelete = prompt("Enter the number of the task you want to delete:\n" + taskListString);
-        taskToDelete = Number(taskToDelete);
-        todo.splice(taskToDelete - 1, 1);
-        alert("Task Deleted!");
-    } else {
-        console.log("Invalid Command!");
+for (let i = 0; i < array.length; i++) {
+    if (array[i] == number) {
+        array.splice(i, 1);
     }
-    req = prompt("Enter Your Request");
 }
+console.log(array);
+
+//Question 2
+/* My Approach */
+number = 287152;
+let count = String(number).length;
+console.log(`Number of Digits in ${number} is ${count}`);
+
+/* Their Approach */
+count = 0;
+let copy = number;
+while (copy > 0) {
+    count++;
+    copy = Math.floor(copy / 10);
+}
+console.log(count);
+
+//Question 3
+/* My Approach */
+let sum = 0;
+let strNum = String(number);
+for (let i = 0; i < strNum.length; i++) {
+    let digit = parseInt(strNum[i]);
+    sum += digit;
+}
+console.log(`Sum of digits in ${number} is ${sum}`);
+
+/* Their Approach */
+sum = 0;
+copy = number;
+while (copy > 0) {
+    digit = copy % 10;
+    sum += digit;
+    copy = Math.floor(copy / 10);
+}
+console.log(sum);
+
+//Question 4
+let factorial = 1;
+number = 7;
+for (let i = 1; i <= number; i++) {
+    factorial *= i;
+}
+console.log(`Factorial of ${number} is ${factorial}.`);
+
+//Question 5
+let largest = -1;
+for (num of array) {
+    if (largest < num) {
+        largest = num;
+    }
+}
+console.log(`The Largest Number of Array if ${largest}.`);
