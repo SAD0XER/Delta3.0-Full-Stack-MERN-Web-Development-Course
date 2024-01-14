4329e2aa6059470f15c3d7ab9e4c44541d2c3b6e
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-01-14 23:29:28 +0530

    Solved JS Assignment Question Set (Part 8)

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,19 +1,50 @@
-// JS Assignment Question Set (Part 7)
+// JS Assignment Question Set (Part 8)
 
 //Question 1
-let array = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+let nums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-let arrayAverage = (array) => {
-    let avg = 0;
-    for (let i = 0; i < array.length; i++) {
-        avg += array[i];
-    }
-    return avg / array.length;
-};
+const square = nums.map((num) => num * num);
+console.log("The square of elements of array is ", square);
 
-console.log(`The Average of given array is ${arrayAverage(array)}`);
+let sum = square.reduce((acc, cur) => acc + cur, 0);
+console.log(`Sum of squares: ${sum}`);
+
+let avg = sum / nums.length;
+console.log("The average of array is", avg);
 
 //Question 2
-const isEven = number => number % 2 == 0;
+let arr = nums.map((num) => num + 5);
+console.log(arr);
 
-console.log(isEven(0));
+//Question 3
+let string = ["abhimanyu", "balaram", "chanakya", "drutrashtra", "ekalavya"];
+
+let toUpperCaseWords = string.map((word) => word.toUpperCase());
+console.log(toUpperCaseWords);
+
+//Question 4
+const doubleAndReturnArgs = (arr, ...args) => [
+    ...arr,
+    ...args.map((v) => v * 2),
+];
+
+doubleAndReturnArgs([1, 2, 3], 4, 4);
+console.log(doubleAndReturnArgs(nums, 123));
+
+doubleAndReturnArgs([2], 10, 4);
+console.log(doubleAndReturnArgs(nums, 1234));
+
+//Question 5
+const obj1 = {
+    name: "asdf",
+    age: 23
+};
+
+const car = {
+    model: "chiron",
+    price: 12345678910
+};
+
+const mergeObjects = (obj1, obj2) => ({ ...obj1, ...obj2 });
+
+console.log(mergeObjects(obj1, car));
