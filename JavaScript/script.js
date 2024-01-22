0a64c0d49c0141d2da9c4a8a37508945c56c7ef9
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-01-22 20:42:33 +0530

    DOM Events and Event Listeners.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,31 +1,27 @@
-// JS Assignment Question Set (Part 9)
+// let btn = document.querySelector('button');
+let btn = document.querySelectorAll('button');
+// console.dir(btn);
 
-// Question 1
-// let body = document.querySelector('body');
-let input = document.createElement("input");
-let btn = document.createElement("button");
-btn.innerText = "Submit!";
-document.querySelector("body").append(input);
-document.querySelector("body").append(btn);
-input.setAttribute("type", "text");
-input.setAttribute("id", "io");
-input.setAttribute("name", "IO");
+// btn.onclick = function() {
+//     console.log("Button Clicked!");
+//     alert("Alert: Button Clicked!");
+// }
 
-// Question 2
-input.setAttribute("placeholder", "username");
-btn.setAttribute("id", "btn");
+function sayHello() {
+    alert("Hello!");
+    console.log("Button Clicked!");
+}
 
-// Question 3
-btn = document.querySelector("#btn");
-btn.classList.add("btnStyle");
+// btn.onclick = sayHello;
 
-// Question 4
-let h1 = document.createElement("h1");
-h1.innerHTML = "<u>DOM Practice</u>";
-document.querySelector("body").append(h1);
-h1.style.color = "purple";
+for (btns of btn) {
+    // btns.onclick = () => { alert("Button Clicked!"); }
+    // btns.onclick = () => { alert("Button Clicked!1"); }
+    // btns.onmouseenter = () => { console.log("Mouse Entered!"); }
+    // btns.onmouseleave = () => { console.log("Mouse Leaved!"); }
 
-// Question 5
-let p = document.createElement("p");
-p.innerHTML = "Apna College <b>Delta</b> Practice";
-document.querySelector("body").append(p);
+    btns.addEventListener("click", sayHello);
+    // btns.addEventListener("dblclick", () => { console.log("Double Cliked!"); });
+    btns.addEventListener("mouseenter", () => { console.log("Mouse Entered!"); });
+    btns.addEventListener("mouseleave", () => { console.log("Mouse Leaved!"); });
+}
