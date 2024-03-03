b0443efabe48923904fe00d60664aa97dcb23053
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-03-03 17:15:05 +0530

    Inheritance in JS.

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -16,7 +16,7 @@ Person.prototype.talk = function () {
 }; */
 
 // 3. Classes: A template for creating objects.
-class Person {
+/* class Person {
   constructor(name, age) { // It's the same as above but using class syntax.
     this.name = name;
     this.age = age;
@@ -28,4 +28,34 @@ class Person {
 }
 
 let person1 = new Person("John", 30);
-person1.talk();
+person1.talk(); */
+
+// OOPs Inheritance in JS
+//Parent class / Base class.
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  walk() {
+    console.log(`I am walking.`);
+  }
+}
+
+//Extending Person class.
+class Student extends Person {
+  constructor(name, age, marks) {
+    //'super' keyword calls the parent constructor.
+    super(name, age); // calling parent constructor.
+    this.marks = marks;
+  }
+
+  // This is a example of overriding.
+  walk() {
+    console.log(`${this.name} is walking.\nThis is overrided function.`);
+  }
+}
+
+let std1 = new Student("sarvesh", 10, 90); //Creating student object.
+console.log(`Name is ${std1.name}.\nAge is ${std1.age}.\nI got ${std1.marks} marks out of 100.`);
+std1.walk();
