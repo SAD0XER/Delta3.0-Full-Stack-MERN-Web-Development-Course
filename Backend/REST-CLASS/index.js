3aaaf3328b4253a0fd6b5c35b4ce69c4d67c7be3
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-03-04 12:48:40 +0530

    Making Quora Page using RESTful APIs: Set the basic structure of server.

---
+++ Backend/REST-CLASS/index.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 8080;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json()); // to support JSON-encoded bodies.
+
+app.set("view engine", "ejs");
+app.set("view engine", path.join(__dirname, "views"));
+app.set(express.static(path.join(__dirname, "/public")));
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}...`);
+})
+
+app.get("/", (req, res) => {
+    res.send("Server is live.");
+});
