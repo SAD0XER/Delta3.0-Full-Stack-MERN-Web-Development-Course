c5b6debc3898c691a87f6d5c65f86ca8fde62fbf
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-03-05 11:06:59 +0530

    Create '/posts' route to get data for all posts.

--- Backend/REST-CLASS/index.js
+++ Backend/REST-CLASS/index.js
@@ -6,10 +6,10 @@ const port = 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); // to support JSON-encoded bodies.
+app.use(express.static(path.join(__dirname, "/public"))); // Serve static files from the public folder.
 
 app.set("view engine", "ejs");
 app.set("view engine", path.join(__dirname, "views"));
-app.set(express.static(path.join(__dirname, "/public")));
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}...`);
@@ -17,4 +17,23 @@ app.listen(port, () => {
 
 app.get("/", (req, res) => {
     res.send("Server is live.");
+});
+
+let posts = [
+    {
+        username: "apnacollege",
+        content: "Do Not Stop unil the goal is reached."
+    },
+    {
+        username: "Aman Dhattarwal",
+        content: "Tu Phodega!"
+    },
+    {
+        username: "Shrasha Khapra",
+        content: "Success requires hardwork."
+    },
+];
+
+app.get("/posts", (req, res) => {
+    res.render("index.ejs", { posts });
 });
