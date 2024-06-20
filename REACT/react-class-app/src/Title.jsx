bf90dcc24baaff0b85ee55ad6751a62030ab8bd5
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-20 16:08:48 +0530

    Styling Components in JSX.

--- REACT/react-class-app/src/Title.jsx
+++ REACT/react-class-app/src/Title.jsx
@@ -1,5 +1,7 @@
-export default function Title() {
-  return <h1>This is the Title.</h1>;
+import "./Title.css"; // This is standard practice to keep import statmements at the TOF.
+
+function Title() {
+  return <h1 className="Title">This is the Title.</h1>;
 }
 
-// export default Title;
+export default Title; // This is standard practice to keep export statements at the EOF.
