c2565df97ce0d62e8c64e47f123519d7d598a0cb
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-20 14:02:04 +0530

    JSX with curly braces.

--- REACT/react-class-app/src/App.jsx
+++ REACT/react-class-app/src/App.jsx
@@ -2,7 +2,14 @@ import "./App.css";
 import Title from "./Title.jsx";
 
 function App() {
-  return <Title />;
+  return (
+    <>
+      <Title />
+      <p style={{ color: "gold", backgroundColor: "darkviolet" }}>
+        This is the paragraph and the sum of 6 * 5 + 2 + 2 is {6 * 5 + 2 + 2}
+      </p>
+    </>
+  );
 }
 
 export default App;
