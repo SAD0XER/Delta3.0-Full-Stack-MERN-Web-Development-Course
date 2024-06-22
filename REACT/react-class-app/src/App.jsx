e09a39bc92566cb953505091d435622d311f4b3e
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-22 16:24:25 +0530

    Activity: To show "Hello <userName>" message to a user in different colors, pass 2 values to props: userName & textColor

--- REACT/react-class-app/src/App.jsx
+++ REACT/react-class-app/src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
 import Product from "./Product.jsx";
+import MsgUser from "./MsgUser.jsx";
 
 function App() {
   return (
     <>
+      <MsgUser userName="Sarvesh" textColor="yellow" />
       <Product title="Phone" price={30000} />
       <Product title="Laptop" price={50000} />
       <Product title="Head Phone" price={10000} />
---
+++ REACT/react-class-app/src/MsgUser.jsx
@@ -0,0 +1,3 @@
+export default function MsgUser({ userName, textColor }) {
+  return <h2 style={{ color: textColor }}>Hello {userName}.</h2>;
+}
