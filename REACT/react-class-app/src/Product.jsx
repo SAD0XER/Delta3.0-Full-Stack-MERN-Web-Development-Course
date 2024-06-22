4fa3af1113ff7d757a40312febb25bf6ce748d1b
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-22 15:50:41 +0530

    Dynamic Component Styling (Conditional Component Styling)

--- REACT/react-class-app/src/Product.jsx
+++ REACT/react-class-app/src/Product.jsx
@@ -1,12 +1,13 @@
 import "./Product.css";
 
 function Product({ title, price = 1 }) {
+  let isDiscount = price > 30000;
+  let style = { backgroundColor: isDiscount ? "darkblue" : "initial" };
   return (
-    <div className="Product">
+    <div className="Product" style={style}>
       <h3>{title}</h3>
       <p>{price}</p>
-      {price > 30000 ? <p>30% Discount</p> : <a href="/">Get Discount</a>}
-      {/* {price > 30000 && <p>30% Discount</p>} */}
+      {isDiscount ? <p>30% Discount</p> : <a href="/">Get Discount</a>}
     </div>
   );
 }
