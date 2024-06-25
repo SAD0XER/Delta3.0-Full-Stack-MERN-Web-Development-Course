663c9c2dd706d08ce4a444fdd89bcf7f7f872241
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-25 15:37:29 +0530

    Events in React: Non-Click Event.

--- REACT/react-class-app/src/ButtonEvent.jsx
+++ REACT/react-class-app/src/ButtonEvent.jsx
@@ -1,11 +1,21 @@
-function buttonClicked() {
-  console.log("times button clicked!");
+function onClick() {
+  console.log("onClick Event");
+}
+
+function onDoubleClick() {
+  console.log("onDoubleClick event");
+}
+
+function onMouseOver() {
+  console.log("onMouseOver Event");
 }
 
 export default function ButtonEvent() {
   return (
     <>
-      <button onClick={buttonClicked}>Click Me!</button>
+      <button onClick={onClick}>Click Me!</button>
+      <p onMouseOver={onMouseOver}>onMouseOverEvent!</p>
+      <button onDoubleClick={onDoubleClick}>Click Me!</button>
     </>
   );
 }
