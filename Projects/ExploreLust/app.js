9dbd1810ab111c6598246db7ef44e3185bf07273
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-06-26 22:11:51 +0530

    Created  Show Route: /listings/:id - To see a single list.

--- Projects/ExploreLust/app.js
+++ Projects/ExploreLust/app.js
@@ -6,6 +6,7 @@ const path = require("path");
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/ExploreLust";
 
@@ -35,4 +36,11 @@ app.get("/", (req, res) => {
 app.get("/listings", async (req, res) => {
   const allListings = await Listing.find({}); //To extract all listing data from database.
   res.render("./listings/index.ejs", { listings: allListings }); //Passing all listings to index.ejs with key name as `listings`.
+});
+
+// Show Route: /listings/:id - To see a single list.
+app.get("/listings/:id", async (req, res) => {
+  const { id } = req.params;
+  const listing = await Listing.findById(id);
+  res.render("./listings/show.ejs", { listing });
 });
