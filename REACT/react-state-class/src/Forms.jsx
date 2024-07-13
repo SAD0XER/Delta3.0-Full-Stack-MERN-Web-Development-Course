63a0a67d0b9b1891e1a5f2be49031d9b673d1c5d
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-07-13 15:55:42 +0530

    Handling Multiple Inputs in forms.

--- REACT/react-state-class/src/Forms.jsx
+++ REACT/react-state-class/src/Forms.jsx
@@ -1,24 +1,50 @@
 import { useState } from "react";
 
 export default function Forms() {
-  let [name, setName] = useState("");
+  let [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
 
-  let handleNameChange = (event) => {
-    setName(event.target.value);
+  let handleInputChange = (event) => {
+    // let fieldName = event.target.name;
+    // let fieldValue = event.target.value;
+
+    setFormData((currData) => {
+      // currData[fieldName] = fieldValue; // This is the alternative method to change the values.
+      return { ...currData, [event.target.name]: event.target.value };
+    });
+  };
+
+  let handleSubmit = (event) => {
+    event.preventDefault();
+    setFormData({ username: "", password: "" }); // Setting empty field to reset after submission.
   };
 
   return (
     <>
-      <form>
-        {/* 'htmlFor' is the alternative to 'for' in normal HTML. */}
-        <label htmlFor="name">Enter Name </label>
+      <h1>Forms in HTML</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="username">Enter Username </label>
         <input
           type="text"
-          id="name"
-          placeholder="Your Name"
-          value={name}
-          onChange={handleNameChange}
+          id="username"
+          placeholder="Enter Username"
+          value={formData.username}
+          onChange={handleInputChange}
+          name="username"
         />
+        <br />
+        <label htmlFor="password">Enter Password </label>
+        <input
+          type="password"
+          id="password"
+          placeholder="Enter Password"
+          value={formData.password}
+          onChange={handleInputChange}
+          name="password"
+        />
+        <br />
         <button>Submit</button>
       </form>
     </>
