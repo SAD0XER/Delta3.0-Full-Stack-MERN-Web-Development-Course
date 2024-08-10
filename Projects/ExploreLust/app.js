e40a7ec0b9c3715b0aa6f8dfc9e691a4895c0d92
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-08-10 21:30:11 +0530

    Creating Boilerplate code using ejs-mate pkg.

--- Projects/ExploreLust/app.js
+++ Projects/ExploreLust/app.js
@@ -3,12 +3,14 @@ const mongoose = require("mongoose");
 const Listing = require("./models/listing.js");
 const path = require("path");
 const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
 
 const app = express();
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/ExploreLust";
 
@@ -81,4 +83,4 @@ app.delete("/listings/:id/delete", async (req, res) => {
     let deleted = await Listing.findByIdAndDelete(id);
     console.log(deleted);
     res.redirect(`/listings`);
-})
+});
