026e998e71e0eab22fc3a82ec35eaa4efeaa86d9
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-10-11 16:45:04 +0530

    Middleware: Starting with our 1st Middleware use().

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -1,4 +1,5 @@
 /* Creating Web Application using Express */
+/* It is a basic Express app, you can use this as template to get started. */
 
 const express = require("express"); //Requiring a Express
 const app = express(); //It is basically a function.
@@ -6,47 +7,24 @@ const app = express(); //It is basically a function.
 let port = 8080; //3000
 
 //Handling Requests: Listen makes web server that listen incoming API requests.
-app.listen(port, () => {
-  console.log(`Server Starts: App is litening on port ${port}`);
+app.listen(8080, () => {
+    console.log(`Server Started: App is litening on port ${port}`);
 });
 
-//Sending Response: This method listens every request and execute callback for response.
-/* app.use((req, res) => {
-  //   console.log(req);
-  console.log("Request Recieved.");
-  res.send({ status: true, data: [] });
-}); */
+// use(): This Middleware listens all kind of every single requests even request is wrong and it executed.
+app.use((req, res) => {
+    // let { id } = req.query; // Extracting parameter from request object.
+    // console.log(id); // Printing extracted parameter value or query value.
+    res.send("Hello, I am Middleware.");
+    console.log(123);
+});
 
 //Routing
 app.get("/", (req, res) => {
-  res.send("Well come to my home page.");
+    res.send("Hello there!");
 });
 
-/* app.get("/project", (req, res) => {
-  console.log("Project response send.");
-  res.send("You contacted Project path.");
-}); */
-
-//To handle another kind of GET requests who are not exist.
-/* app.get("*", (req, res) => {
-  res.send("page/path does not exist.");
-}); */
-
-//Path Parameters: 'req.params' has parameters passed by client.
-app.get("/:userName/:userID", (req, res) => {
-  // console.log(req.params);
-  let { userName, userID } = req.params;
-  res.send(`Hello, @${userName}, and your ID is ${userID}`);
-});
-
-//Query String
-app.get("/search", (req, res) => {
-  console.log(req.query);
-  let { q, q2 } = req.query;
-
-  //If query does not found.
-  if (!q) {
-    res.send("No query found.");
-  }
-  res.send(`Your queries are ${q} ${q2}`);
+app.get("/project", (req, res) => {
+    console.log("Project response send.");
+    res.send("You contacted Project path.");
 });
