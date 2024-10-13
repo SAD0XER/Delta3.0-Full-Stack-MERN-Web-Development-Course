9bd1d3c8e1612f57ac5c0163631da5e35042d5ab
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-10-13 22:27:44 +0530

    Middleware using next().

--- Backend/Express/index.js
+++ Backend/Express/index.js
@@ -6,18 +6,30 @@ const app = express(); //It is basically a function.
 
 let port = 8080; //3000
 
-//Handling Requests: Listen makes web server that listen incoming API requests.
-app.listen(8080, () => {
-    console.log(`Server Started: App is litening on port ${port}`);
-});
-
 // use(): This Middleware listens all kind of every single requests even request is wrong and it executed.
-app.use((req, res) => {
-    // let { id } = req.query; // Extracting parameter from request object.
-    // console.log(id); // Printing extracted parameter value or query value.
-    res.send("Hello, I am Middleware.");
-    console.log(123);
-});
+app.use(
+    (req, res, next) => {
+        console.log(1.1);
+        next(); // This line is important to prevent the request from being stuck in the middleware.
+        console.log("after1.1"); // Writing code after next() is not a good practice.
+    },
+    (req, res, next) => {
+        console.log(1.2);
+        return next(); // Developers returns the next(), so code after the next() should not run.
+        console.log("after1.2"); // Writing code after next() is not a good practice.
+    },
+    (req, res, next) => {
+        console.log(1.3);
+        next();
+        console.log("after1.3");
+    },
+);
+
+app.use((req, res, next) => {
+    console.log(2.1);
+    next();
+    console.log("after2.1");
+}); // Observe the output of this code.
 
 //Routing
 app.get("/", (req, res) => {
@@ -27,4 +39,9 @@ app.get("/", (req, res) => {
 app.get("/project", (req, res) => {
     console.log("Project response send.");
     res.send("You contacted Project path.");
+});
+
+//Handling Requests: Listen makes web server that listen incoming API requests.
+app.listen(8080, () => {
+    console.log(`Server Started: App is litening on port ${port}`);
 });
