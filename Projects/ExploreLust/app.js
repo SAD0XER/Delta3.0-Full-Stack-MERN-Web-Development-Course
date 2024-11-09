3e0fa26d2cfbd93030ecb4ae273b9d02e92d8c98
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-11-09 17:01:13 +0530

    Added Error.ejs to show proper styled error message on the screen.

--- Projects/ExploreLust/app.js
+++ Projects/ExploreLust/app.js
@@ -107,7 +107,7 @@ app.all("*", (req, res, next) => {
 
 app.use((err, req, res, next) => {
     let { statusCode = 500, message = "Something Went Wrong!" } = err;
-    res.status(statusCode).send(message);
+    res.status(statusCode).render("error.ejs", { message });
 });
 
 /* Server setup */
