6a57c1682e6ea9865d45a88b7c6f5ff6ec947690
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-14 21:35:24 +0530

    Signed Cookies: Used to know cookie got compromised/tempered or not.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -6,7 +6,7 @@ const port = 8080;
 
 app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(express.json()); // parse application/json
-app.use(cookieParser()); // Cookie Parser Middleware.
+app.use(cookieParser("encryption-code")); // Cookie Parser Middleware.
 
 app.get("/", (req, res) => {
     console.dir(req.cookies); // Printing cookie data.
@@ -24,6 +24,19 @@ app.get("/getcookie", (req, res) => {
 app.get("/getcookiedata", (req, res) => {
     let { name = "nothing" } = req.cookies;
     res.send(`This cookie has ${name}`);
+});
+
+// Using cookies on the web page.
+app.get("/getsignedcookie", (req, res) => {
+    res.cookie("Made-In", "Bharat", { signed: true });
+    res.send("You got some Signed Cookies.");
+});
+
+// Using cookies on the web page.
+app.get("/verifycookie", (req, res) => {
+    console.log(req.cookies); // It will print unsigned cookies.
+    console.log(req.signedCookies); // It will print signed cookies.
+    res.send("Demo for verifying cookies.")
 });
 
 app.get("/register", (req, res) => {
