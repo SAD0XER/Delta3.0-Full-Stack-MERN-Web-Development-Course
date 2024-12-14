e390a961a3169b30a535a4ca09487e7975a0e6d6
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-14 20:55:37 +0530

    Cookie Parser: Using cookie data on the web page.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -1,19 +1,29 @@
 const express = require("express");
 const app = express();
+const cookieParser = require("cookie-parser");
 
 const port = 8080;
 
 app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(express.json()); // parse application/json
+app.use(cookieParser()); // Cookie Parser Middleware.
 
 app.get("/", (req, res) => {
+    console.dir(req.cookies); // Printing cookie data.
+    console.log(req.cookies); // Printing cookie data.
     res.send("Hello, I am Root.")
 });
 
+// Storing cookies on browser.
 app.get("/getcookie", (req, res) => {
     res.cookie("name", "value");
-    res.cookie("sarvesh", "devrukhakar");
     res.send("You got some Cookies.");
+});
+
+// Using cookies on the web page.
+app.get("/getcookiedata", (req, res) => {
+    let { name = "nothing" } = req.cookies;
+    res.send(`This cookie has ${name}`);
 });
 
 app.get("/register", (req, res) => {
