e5539dfde89224491ccba00e81446af04601e734
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-14 20:38:08 +0530

    Sending Cookies.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -6,6 +6,16 @@ const port = 8080;
 app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
 app.use(express.json()); // parse application/json
 
+app.get("/", (req, res) => {
+    res.send("Hello, I am Root.")
+});
+
+app.get("/getcookie", (req, res) => {
+    res.cookie("name", "value");
+    res.cookie("sarvesh", "devrukhakar");
+    res.send("You got some Cookies.");
+});
+
 app.get("/register", (req, res) => {
     console.log(req.query);
     let { username, password } = req.query; //
