f4519aad8e7cdc04c7a472ed74cba9b05af7df45
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-15 17:06:02 +0530

    Express Session using npm pkg 'express-session'.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -1,57 +1,14 @@
 const express = require("express");
 const app = express();
-const cookieParser = require("cookie-parser");
-
+const session = require("express-session");
 const port = 8080;
 
-app.use(express.urlencoded({ extended: true })); // parse application/x-www-form-urlencoded
-app.use(express.json()); // parse application/json
-app.use(cookieParser("encryption-code")); // Cookie Parser Middleware.
+app.use(session({ secret: "BishopTakesRook" }));
 
-app.get("/", (req, res) => {
-    console.dir(req.cookies); // Printing cookie data.
-    console.log(req.cookies); // Printing cookie data.
-    res.send("Hello, I am Root.")
-});
-
-// Storing cookies on browser.
-app.get("/getcookie", (req, res) => {
-    res.cookie("name", "value");
-    res.send("You got some Cookies.");
-});
-
-// Using cookies on the web page.
-app.get("/getcookiedata", (req, res) => {
-    let { name = "nothing" } = req.cookies;
-    res.send(`This cookie has ${name}`);
-});
-
-// Using cookies on the web page.
-app.get("/getsignedcookie", (req, res) => {
-    res.cookie("Made-In", "Bharat", { signed: true });
-    res.send("You got some Signed Cookies.");
-});
-
-// Using cookies on the web page.
-app.get("/verifycookie", (req, res) => {
-    console.log(req.cookies); // It will print unsigned cookies.
-    console.log(req.signedCookies); // It will print signed cookies.
-    res.send("Demo for verifying cookies.")
-});
-
-app.get("/register", (req, res) => {
-    console.log(req.query);
-    let { username, password } = req.query; //
-    res.send(`Standard GET response. Welcome ${username}!`);
-});
-
-app.post("/register", (req, res) => {
-    console.log(req.body);
-    let { username, password } = req.body;
-    res.send(`Standard GET response. Welcome ${username}!`);
+app.get("/test", (req, res) => {
+    res.send("Test successful!");
 });
 
 app.listen(port, () => {
     console.log(`Server is started on port ${port}`);
 });
-
