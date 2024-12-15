8fc431992087d243dcd481ffb029b80218952769
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-15 17:42:32 +0530

    Stateful Session: Exploring Session Options.
    It is a practical example of **Stateful Session**.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -3,10 +3,12 @@ const app = express();
 const session = require("express-session");
 const port = 8080;
 
-app.use(session({ secret: "BishopTakesRook" }));
+app.use(session({ secret: "BishopTakesRook", resave: false, saveUninitialized: true }));
 
-app.get("/test", (req, res) => {
-    res.send("Test successful!");
+app.get("/requestcount", (req, res) => {
+    if (req.session.count) req.session.count++;
+    else req.session.count = 1; // If 'count' variable doesn't exit in the 'req.session', then create and initialize it.
+    res.send(`Sending Request for ${req.session.count} times.`);
 });
 
 app.listen(port, () => {
