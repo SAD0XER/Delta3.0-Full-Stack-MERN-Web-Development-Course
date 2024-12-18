61b4da8cb8beb1d6e9036e9ac084c30812d18c63
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-18 22:07:38 +0530

    Showed temporary message on web page using npm package connect-flash.

--- Backend/MISCELLANEOUS/BACKEND/index.js
+++ Backend/MISCELLANEOUS/BACKEND/index.js
@@ -1,20 +1,27 @@
 const express = require("express");
-const app = express();
 const session = require("express-session");
+const flash = require("connect-flash");
+const path = require("path");
+const app = express();
 const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
 
 const sessionOption = { secret: "BishopTakesRook", resave: false, saveUninitialized: true };
 
 app.use(session(sessionOption));
+app.use(flash());
 
 app.get("/register", (req, res) => {
     let { name = "NoName" } = req.query;
     req.session.name = name;
+    req.flash("success", "User registered successfully!");
     res.redirect("/hello");
 });
 
 app.get("/hello", (req, res) => {
-    res.send(`Hello, ${req.session.name}`);
+    res.render("./hello.ejs", { name: req.session.name, flashMsg: req.flash("success") });
 });
 
 app.listen(port, () => {
