aff070fd5c79dca80162b52202230a23462f8369
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2024-12-20 15:41:29 +0530

    Cookies in `sessionOptions`
    Added cookie expiry date.

--- Projects/ExploreLust/app.js
+++ Projects/ExploreLust/app.js
@@ -26,6 +26,11 @@ const sessionOptions = {
     secret: "Session Secret",
     resave: false,
     saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        maxAge: Date.now() + 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+    },
 };
 
 app.use(session(sessionOptions));
