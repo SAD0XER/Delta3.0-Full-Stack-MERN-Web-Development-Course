b9ad26af27012d0086e1d8849a0f7590136b63de
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2025-01-08 16:08:01 +0530

    SignUp User - POST /signup
    Creating POST route to save user information in database.

--- Projects/ExploreLust/routes/user.js
+++ Projects/ExploreLust/routes/user.js
@@ -1,8 +1,27 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
+const User = require("../models/user.js");
+const wrapAsync = require("../utils/wrapAsync.js");
 
 router.get("/signup", (req, res) => {
     res.render("users/signup.ejs");
 });
+
+router.post(
+    "/signup",
+    wrapAsync(async (req, res) => {
+        try {
+            let { username, email, password } = req.body;
+            const newUser = new User({ email, username });
+            const registeredUser = await User.register(newUser, password);
+            console.log(registeredUser);
+            req.flash("success", "Welcome to ExploreLust!");
+            res.redirect("/listings");
+        } catch (error) {
+            req.flash("error", error.message);
+            res.redirect("/signup");
+        }
+    }),
+);
 
 module.exports = router;
