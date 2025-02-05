9830774a54adb766c587a608e8fb74f924c99800
PARA_C0DER <79737447+SAD0XER@users.noreply.github.com>
2025-02-05 22:03:42 +0530

    Cloud Setup.

--- Projects/ExploreLust/app.js
+++ Projects/ExploreLust/app.js
@@ -1,3 +1,6 @@
+// Check for NODE_ENV is not set on "production".
+if (process.env.NODE_ENV != "production") require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 const path = require("path");
